9a0ac2b99876c52045603da32a5cddc7fe72031f
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2025-02-05 21:07:13 +0200

    new background redesign, updated slider stlying

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import "./index.css";
 import Header from "./components/Header.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Header />
-    <App />
-  </BrowserRouter>,
+  <>
+    <BrowserRouter>
+      <Header />
+      <App />
+      <div className="grain"></div>
+    </BrowserRouter>
+  </>
 );
